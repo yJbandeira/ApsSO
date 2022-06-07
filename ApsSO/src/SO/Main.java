d419db65fe446dfda6c409a27c45b79420ce8b6d
João Bandeira <joaoml.victor13@gmail.com>
2022-06-06 21:32:52 -0300

    Commit inicial, com upload de arquivo

---
+++ ApsSO/src/SO/Main.java
@@ -0,0 +1,20 @@
+package SO;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		File file = new File("/aps/entrada.txt");
+		
+		Scanner scan = new Scanner(file);
+		
+		while(scan.hasNextLine()) {
+			System.out.println(scan.nextLine());
+		}
+
+	}
+
+}
